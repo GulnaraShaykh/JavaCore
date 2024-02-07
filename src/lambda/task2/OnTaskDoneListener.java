1e7ce59d32dcb0d8f17b41d3c5a3be3b2e6f5ea0
Gulnara Shaikhutdinova <gu_code@mail.ru>
2024-02-07 03:21:20 +0300

    tst

--- src/lambda/task2/OnTaskDoneListener.java
+++ src/lambda/task2/OnTaskDoneListener.java
@@ -1,5 +1,7 @@
 package lambda.task2;
 
+
+@FunctionalInterface
 public interface OnTaskDoneListener {
     void onDone(String result);
 }
--- src/lambda/task2/OnTaskErrorListener.java
+++ src/lambda/task2/OnTaskErrorListener.java
@@ -1,5 +1,7 @@
 package lambda.task2;
 
+
+@FunctionalInterface
 public interface OnTaskErrorListener {
     void onError(String result);
 }
